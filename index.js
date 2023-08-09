a1f18165da3049d9b0ef7e04932cbc72a1e2645b
KARTHIKEYAN R <r.karthikeyansmart@gmail.com>
2023-08-09 20:51:12 +0530

    today's lesson about css and javascript added

--- index.js
+++ index.js
@@ -1,37 +1,40 @@
-console.log("hello!");
-//numbers
-var num1 = 7;
-var num2 = 78;
-//strings
-var str1 = "tiger";
-var str2 = "lion";
-//objects
-var marks = {
-    ram: 55,
-    sham: 7,
-    lakshman: 8
-}
-// console.log(marks, str1, str2, num1, num2);
-//boolean 
-var b1=true;
-var b2=false;
-console.log(b1,b2);
+// console.log("hello!");
+// //numbers
+// var num1 = 7;
+// var num2 = 78;
+// //strings
+// var str1 = "tiger";
+// var str2 = "lion";
+// //objects
+// var marks = {
+//     ram: 55,
+//     sham: 7,
+//     lakshman: 8
+// }
+// // console.log(marks, str1, str2, num1, num2);
+// //boolean 
+// var b1=true;
+// var b2=false;
+// console.log(b1,b2);
 
-var und;
-var u=undefined;
-var n=null;
-console.log(und,u);
-console.log(n);
-//arrays
-var arr=[1,2,"hello",true,5.6];
+// var und;
+// var u=undefined;
+// var n=null;
+// console.log(und,u);
+// console.log(n);
+// //arrays
+// var arr=[1,2,"hello",true,5.6];
+// console.log(arr);
+// console.log(arr[0]);
+// console.log(arr[3]);
+// var number=8;
+// number=number+1;
+// number++;
+// console.log(number);
+// console.log(!true);
+// console.log(true && false);
+// console.log(true || false);
+
+let arr=[5,9.4,"array",'c',true,null];
 console.log(arr);
 console.log(arr[0]);
-console.log(arr[3]);
-var number=8;
-number=number+1;
-number++;
-console.log(number);
-console.log(!true);
-console.log(true && false);
-console.log(true || false);
-
---
+++ tutorial.js
@@ -0,0 +1,6 @@
+console.log("hello world");
+let arr=[1,2,3];
+console.log(arr);
+arr.pop();
+console.log(arr);
+arr.push("karthik");
